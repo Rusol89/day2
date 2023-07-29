dd5046b1e7e3b50a75b6a9c8c1eb3c5ab3320a72
Rusol89 <140642360+Rusol89@users.noreply.github.com>
2023-07-29 10:50:27 -0400

    Update DemoDay2.java

--- src/Day2/DemoDay2.java
+++ src/Day2/DemoDay2.java
@@ -8,5 +8,9 @@ public static void main(String[] args) {
 public static void subtract(int a , int b) {
 	System.out.println(a-b);
 }
+	// add method
+public static void subtract(int a , int b , int c ) {
+	System.out.println(a-b-c);
+}
 }
 
